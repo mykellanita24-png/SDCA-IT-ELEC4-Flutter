09bf1f81e5db45cede49df0b014ec63ff7db5dd4
Myke Allen Llanita <mykellanita24@gmail.com>
2025-09-06 15:53:06 +0800

    Initial Flutter code

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,29 @@
 import 'package:flutter/material.dart';
 
 void main() {
-runApp(App());
+  runApp(App());
 }
 
 class App extends StatelessWidget {
- @override
- Widget build(BuildContext context) {
-   Scaffold homepage = Scaffold(
-    appBar: AppBar(title: Text('Homepage'))
-   );
+  @override
+  Widget build(BuildContext context) {
+    Scaffold homepage = Scaffold(
+      appBar: AppBar(title: Text('Homepage')),
+      floatingActionButton: FloatingActionButton
+      (onPressed: () {
 
-   return MaterialApp(
-    home: homepage,
-    theme: ThemeData(
-      appBarTheme: AppBarTheme(
-        backgroundColor: Colors.red,
-        foregroundColor: Colors.white
-      )
-    )
-   ); // MaterialApp
- }
+      },
+      child: const Icon(Icons.add)
+      ),
+    );
+    return MaterialApp(
+      home: homepage,
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.red,
+          foregroundColor: Colors.white
+        )
+      ),
+    );
+  }
 }
-
